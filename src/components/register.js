201ce67a7f16ad4bf19a5a6beb70520897e8895b
Hunter K <huntertkoehn@gmail.com>
2023-07-08 09:16:53 -0500

    tweaked register component css

--- src/components/register.js
+++ src/components/register.js
@@ -14,17 +14,17 @@ export const Register = (props) => {
         <div className='auth-form-container'>
         <form onSubmit={handleSubmit}>
 
-                <div className='form-group'>
+                <div className='form-group register-name'>
                     <label htmlFor='name'>Name</label>
                     <input value={name} onChange={(e) => setName(e.target.value)} type='text' placeholder='First and last name' id='name' name='name'></input>
                 </div>
                 
-                <div className='form-group'>
+                <div className='form-group register-username'>
                     <label htmlFor='user'>Username</label>
                     <input value={user} onChange={(e) => setUser(e.target.value)} type='text' placeholder='Desired username' id='user' name='user'></input>
                 </div>
                 
-                <div className='form-group'>
+                <div className='form-group register-password'>
                     <label htmlFor='password'>Password</label>
                     <input value={pass} onChange={(e) => setPass(e.target.value)} type='password' placeholder='Desired password' id='password' name='password'></input>
                 </div>
@@ -34,7 +34,7 @@ export const Register = (props) => {
                 </div>
                 
             </form>
-            <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+            <button className='register-now-btn' onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
         </div>
     )
 }
